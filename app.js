99835f07f66c1d531c33efd14ec2226e1e9f3683
simee56 <pandeysimee7@gmail.com>
2024-12-21 22:08:27 +0530

    correct errors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("ist-container");
+const listContainer = document.getElementById("list-container");
 
 function addTask(){
     if(inputBox.value === ''){
